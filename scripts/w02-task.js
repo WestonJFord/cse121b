cf3d89492eca00c58e5cb3689b59f4fc49601a6b
WestonJford <westonford@outlook.com>
2023-09-23 09:09:11 -0600

    w01 complete

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,27 +3,29 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
+let fullName = 'Weston Ford';
+let currentYear = new Date().getFullYear();
+let profilePicture = 'images/weston_ford.jpg';
 
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
-
-
-
-
-
-
+let favoriteFoods = ['lasagna', 'pizza', 'steak', 'loaded potatoes', 'crepes'];
+foodElement.innerHTML = favoriteFoods;
+favoriteFoods.push('ice cream')
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
