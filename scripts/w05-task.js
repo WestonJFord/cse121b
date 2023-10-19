18c9bf277ed5d8e30cfe0d48a4a65fe35ef454ef
WestonJford <westonford@outlook.com>
2023-10-18 18:10:46 -0600

    idc

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -3,40 +3,67 @@
 /* Declare and initialize global variables */
 
 const templesElement = document.getElementById('temples');
-var templesList = [];
+const templeList = [];
 
 /* async displayTemples Function */
 
-const displayTemples = () => {
-    templesList.forEach(temple => {
+const displayTemples = (temples) => {
+    temples.forEach(temple => {
         let article = document.createElement('article');
         let h3 = document.createElement('h3');
         h3.textContent = temple.templeName;
         let img = document.createElement('img');
         img.src = temple.imageUrl;
         img.alt = temple.location;
-        article.appendChild(h3, img);
+        article.appendChild(h3);
+        article.appendChild(img)
         templesElement.appendChild(article);
     });
-}
+    console.log(templesElement)
+};
 
 /* async getTemples Function using fetch()*/
 
 const getTemples = async () => {
     const response = await fetch('https://byui-cse.github.io/cse121b-ww-course/resources/temples.json');
     if (response.ok) {
-        let temples = await response.json();
-        templesList.push(temples);
-    }
-}
+        templeList.push(await response.json());
+    };
+};
 
 /* reset Function */
 
+function reset() {
+    templesElement.remove();
+};
 
 /* sortBy Function */
 
-
+function sortBy(temples) {
+    reset();
+    let filter = document.querySelector('#sortBy');
+    switch(filter.value) {
+        case 'utah':
+            let utahTemples = temples.filter(temple => temple.location.includes('Utah'));
+            displayTemples(utahTemples);
+            // console.log(utahTemples);
+            break;
+        case 'notutah':
+            let notUtahTemples = temples.filter(temple => !temple.location.includes('Utah'));
+            displayTemples(notUtahTemples);
+            // console.log(notUtahTemples);
+            break;
+        case 'dedicated':
+            break;
+        case 'all':
+            displayTemples(temples);
+            break;
+    };
+};
 
 getTemples();
+console.log(templeList[0]);
 
 /* Event Listener */
+
+document.querySelector("#sortBy").addEventListener("change", () => { sortBy(templeList[0]) });
